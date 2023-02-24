681fd9f908671a3d0d55cebc22a1b90185306d08
academeg1 <m-ignatovich@hotmail.com>
2023-02-24 11:52:24 +0300

    feat: done task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
+    midPointer = 0;
     constructor() {}
 
     setRange(min, max) {
-
+        this.minNumber = min;
+        this.maxNumber = max;
     }
 
     guess() {
-
+        this.midPointer = Math.round((this.maxNumber - this.minNumber) / 2) + this.minNumber;
+        return this.midPointer;
     }
 
     lower() {
-
+        this.maxNumber = this.midPointer;
     }
 
     greater() {
-
+        this.minNumber = this.midPointer;
     }
 }
 
